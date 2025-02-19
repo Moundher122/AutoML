7a3f5e0578b11f09fb793058d44947af445ce3fb
moundher122 <m.bouroumana@esi-sba.dz>
2025-02-19 16:04:15 +0100

    doing some ui

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title('Hello user!')
-
+page = st.sidebar.radio("", [ "Models","New Model", "Profile"])
+if page == "New Model":
+    st.header("📈 Create a new model")
+    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+    if uploaded_file is not None:
+      df = pd.read_csv(uploaded_file)
+      st.write("📊 Preview of the uploaded file:")
+      st.dataframe(df)
+    st.text_input('entre target collumn name')
+    option = st.selectbox("Select a category:", ["Regression", "Classification"])
+    st.write('You selected:', option)
+    if st.button('Create Model'):
+       page = "Models"
+       st.rerun()
